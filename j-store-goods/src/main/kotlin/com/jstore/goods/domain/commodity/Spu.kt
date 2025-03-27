f2effb55f9cc30b860ad7e39d887155ba1088987
pansf <pan102887@qq.com>
2025-03-28 00:32:43 +0800

    将库存模型调整了一下

--- j-store-goods/src/main/kotlin/com/jstore/goods/domain/commodity/Spu.kt
+++ j-store-goods/src/main/kotlin/com/jstore/goods/domain/commodity/Spu.kt
@@ -2,9 +2,15 @@ package com.jstore.goods.domain.commodity
 
 
 import com.jstore.common.errors.BusinessError
+import com.jstore.common.errors.CommonBusinessError
+import com.jstore.common.framework.DomainEvent
+import com.jstore.common.framework.DomainEventPublisher
 import com.jstore.common.framework.Entity
 import com.jstore.common.properties.Id
-import com.jstore.common.utils.Result
+import com.jstore.common.utils.*
+import com.jstore.goods.domain.commodity.event.CommodityOffSaleEvent
+import com.jstore.goods.domain.commodity.event.CommodityOnSaleEvent
+import com.jstore.goods.domain.commodity.event.CommodityPublishedEvent
 
 
 class SpuId(override val value: Long) : Id<Long>(value)
@@ -29,6 +35,62 @@ interface Spu : Entity<SpuId> {
      * 发布
      */
     fun publish(): Result<Boolean, BusinessError>
+}
+
+class SpuImpl(
+    override val id: SpuId,
+    var status: CommodityStatus,
+    val name: String,
+    val skus: MutableList<Sku>,
+    private val domainEventPublisher: DomainEventPublisher,
+) : Spu {
+
+    private val eventBuffer = DomainEventBuffer()
+
+    override fun addSku(sku: Sku): Result<Boolean, BusinessError> {
+        skus.add(sku)
+        return Success(true)
+    }
+
+    override fun putOnSale(): Result<Boolean, BusinessError> {
+        if (CommodityStatus.ON_SALE == status) {
+            return Success(true)
+        }
+        if (CommodityStatus.DRAFT == status) {
+            return Failure(CommonBusinessError.ILLEGAL_STATE.msg("current commodity is in draft, can not operate!!"))
+        }
+        this.status = CommodityStatus.ON_SALE
+        domainEventPublisher.publishEvent(CommodityOnSaleEvent(this, this.id))
+        return Success(true)
+    }
+
+    override fun tackOffSale(): Result<Boolean, BusinessError> {
+        if (CommodityStatus.OFF_SALE == status) {
+            return Success(true)
+        }
+        if (CommodityStatus.DRAFT == status) {
+            return Failure(CommonBusinessError.ILLEGAL_STATE.msg("current commodity is in draft, can not operate!!"))
+        }
+
+        this.status = CommodityStatus.OFF_SALE
+        domainEventPublisher.publishEvent(CommodityOffSaleEvent(this, this.id))
+        return Success(true)
+    }
+
+    override fun publish(): Result<Boolean, BusinessError> {
+        if (CommodityStatus.ON_SALE == status || CommodityStatus.OFF_SALE == status) {
+            return Success(true)
+        }
+        verifyBeforePublish().onFailure { return Failure(it) }
+        this.status = CommodityStatus.OFF_SALE
+        domainEventPublisher.publishEvent(CommodityPublishedEvent(this, id))
+        return Success(true)
+    }
+
+    private fun verifyBeforePublish(): Result<Boolean, BusinessError> {
+        return Success(true)
+    }
+
 }
 
 
--- j-store-goods/src/main/kotlin/com/jstore/goods/domain/commodity/SpuFactory.kt
+++ j-store-goods/src/main/kotlin/com/jstore/goods/domain/commodity/SpuFactory.kt
@@ -1,5 +1,7 @@
 package com.jstore.goods.domain.commodity
 
+import com.jstore.common.framework.DomainEventPublisher
+import com.jstore.common.persistent.SnowFlakSequence
 import com.jstore.goods.domain.commodity.comand.SKUAppendCMD
 import com.jstore.goods.domain.commodity.comand.CommodityCreateCmd
 import org.springframework.stereotype.Component
@@ -14,17 +16,34 @@ interface SpuFactory {
 }
 
 @Component
-class SpuFactoryImpl : SpuFactory {
+class SpuFactoryImpl(
+    private val snowFlakSequence: SnowFlakSequence,
+    private val domainEventPublisher: DomainEventPublisher,
+) : SpuFactory {
     override fun create(createCmd: CommodityCreateCmd): Spu {
-        TODO("Not yet implemented")
+        return SpuImpl(
+            id = SpuId(snowFlakSequence.nextId()),
+            domainEventPublisher = domainEventPublisher,
+            status = CommodityStatus.DRAFT,
+            skus = ArrayList()
+        )
     }
 
     override fun update(createCmd: CommodityCreateCmd, old: Spu): Spu {
-        TODO("Not yet implemented")
+        val spuImpl = SpuImpl(
+            id = old.id,
+            status = CommodityStatus.DRAFT,
+            domainEventPublisher = domainEventPublisher,
+            skus = ArrayList(),
+        )
+        if (old is SpuImpl) {
+            spuImpl.status = old.status
+        }
+        return spuImpl
+
     }
 
     override fun createSKU(skuAppendCMD: SKUAppendCMD): List<Sku> {
         TODO("Not yet implemented")
     }
-
 }
