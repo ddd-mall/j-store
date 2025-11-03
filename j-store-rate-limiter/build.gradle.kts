8bd8b6973631b69d2d5e1ce52547b2a449684d56
pansf <pan102887@qq.com>
2025-11-04 00:28:00 +0800

    添加多数据源配置，整合订单和商品模块，更新应用配置文件

--- j-store-rate-limiter/build.gradle.kts
+++ j-store-rate-limiter/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 
 dependencies {
     implementation(libs.redisson.spring.boot.starter)
-    implementation(platform { libs.spring.cloud.alibaba.dependencies })
+        implementation(platform(libs.spring.cloud.alibaba.dependencies))
     implementation(libs.spring.cloud.starter.alibaba.sentinel)
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
